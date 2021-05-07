ea90b286b8c22bf5d689f7267cce6ba934dc9a49
Adrian Lindell <adrianlindell412@gmail.com>
2021-05-06 21:37:17 -0700

    added login, sign up, camera image selection

--- instagram/AppDelegate.swift
+++ instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "BGoNfewMfQcFMPMqJa4DNsnx53ntk8yOerjhwBqr"
+                    $0.clientKey = "BBEfUz791SMzMjhofs7Ozx0THRiTCrCQh2XJbYl5"
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
